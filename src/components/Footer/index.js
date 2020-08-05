71bbfd55bf1726edafff9c61326bd810451b5dde
Kaic Carvalho <kaic15_fernandes@hotmail.com>
2020-08-05 11:11:19 -0300

    Commit atualizacao das telas

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -11,7 +11,7 @@ function Footer() {
         Desenvolvido por KAIC durante a
         {' '}
         <a href="https://www.alura.com.br/">
-          Imersão React da Alura
+          Imersão React da Alura.
         </a>
       </p>
     </FooterBase>
